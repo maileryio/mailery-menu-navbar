9ec4dedce8dec4c1a60460ac56cb83dacaddbf6c
Igor Maliy <imaliy.filsh@gmail.com>
2021-01-30 23:21:18 +0200

    Development

--- config/web.php
+++ config/web.php
@@ -14,14 +14,12 @@ use Mailery\Menu\Menu;
 use Mailery\Menu\Navbar\NavbarMenuInterface;
 use Mailery\Menu\Decorator\Normalizer;
 use Mailery\Menu\Decorator\Instantiator;
-use Mailery\Menu\Decorator\Sorter;
 use Yiisoft\Injector\Injector;
 
 return [
     NavbarMenuInterface::class => static function (Injector $injector) use($params) {
         return (new Menu($params['maileryio/mailery-menu-navbar']['items']))
-            ->withSorter(new Sorter())
             ->withNormalizer(new Normalizer($injector))
-            ->withInstantiator(new Instantiator());
+            ->withInstantiator(new Instantiator($injector));
     },
 ];
